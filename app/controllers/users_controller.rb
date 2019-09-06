0c2cb68a7539f1bfd1c8dfc26ef7d892990b4b43
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-06 19:24:31 +0100

    Add extra fields to users table, update users form with new fields, update the users index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    include UsersHelper
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.build(user_params)
+        redirect_to user_path(@user)
+    end
+
+    def destroy
+        @user = User.find(params[:id])    
+        @user.destroy
+        flash.notice = 'User was destroyed successfully'
+        redirect_to users_path
+    end
 end
