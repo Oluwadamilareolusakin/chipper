bf5bed6a17a43149c4d451ec31e2810851d11c59
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-07 21:42:19 +0100

    Write failing user tests and validations to make them pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :age, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :gender, presence: true
+    validates :nationality, presence: true
+    validates :username, presence: true, length: { maximum: 15 }
 end
