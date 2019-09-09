7095528f0134a7b70149740107d889967d43c15a
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-09 16:37:46 +0100

    Add minitest and minitest reporters, write administrator controller test for index, show, new and edit

--- app/helpers/administrators_helper.rb
+++ app/helpers/administrators_helper.rb
@@ -1,6 +1,6 @@
 module AdministratorsHelper
     # Never trust parameters from the scary internet, only allow the white list through.
     def administrator_params
-        params.require(:administrator).permit(:email, :username, :password, :name)
+        params.require(:administrator).permit(:email, :username, :name)
     end
 end
