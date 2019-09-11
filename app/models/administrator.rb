ef6cbd4eb46111feb9338f2916dfc09a6e2f4c2e
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-11 20:33:46 +0100

    remove validations for age, nationality and gender from users signup, change new user path to sign up path, add regex for username to admin and user, add email regex to user

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -2,8 +2,9 @@ class Administrator < ApplicationRecord
   authenticates_with_sorcery!
   VALID_PASSWORD_FORMAT = /\A((?=.+[a-z])(?=.+[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]))+/i
   VALID_EMAIL_FORMAT = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-
-  validates :username, presence: true, length: { maximum: 15 }, uniqueness: true
+  VALID_USERNAME = /\A(?![.])(?![\s]{2})[a-zA-z_.0-9]+(?![.])\z/i
+  
+  validates :username, presence: true, length: { maximum: 15 }, uniqueness: true, format: { with: VALID_USERNAME }
   validates :password, presence: true, format: { with: VALID_PASSWORD_FORMAT }, length: {minimum: 7}
   validates :email, presence: true, format: { with: VALID_EMAIL_FORMAT }, uniqueness: true
   validates :name, presence: true
