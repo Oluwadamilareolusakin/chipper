01e6b98f0776d26dabb9460aee0affa84120d520
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-14 00:09:30 +0100

    Precompile assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def page_title(title = '')
+        @base_title = 'Chipper'
+        if title == ''
+            @base_title
+        else
+            title + ' | ' + @base_title
+        end
+    end
 end
