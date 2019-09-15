5572fcc2b9abcb1aa3fc2d85a73f2d3dc6f2baec
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-15 19:17:26 +0100

    Upgrade rails to v 6.0.0 and rewrite routes

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'minitest-reporters'
 gem 'sass-rails', '~> 5.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.6'
+gem 'rails', '6.0.0'
 
 # Adds functionality like assert_template
 gem 'rails-controller-testing'
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'timelines/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#landing_page'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-  resources :users
-  
-  resources :administrators
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users
+  resources :administrators
 end
