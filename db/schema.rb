a728c72796b8d3e0ff642eb8448dd618faedb7aa
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-16 16:07:14 +0100

    Add remember digest to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_225559) do
+ActiveRecord::Schema.define(version: 2019_09_16_145106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_09_15_225559) do
     t.string "gender"
     t.string "nationality"
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
   add_foreign_key "posts", "users"
