15d704f41beac6bb5632bc03c357b33788e4692f
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-16 02:39:47 +0100

    Redirect to timeline if user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
     include SessionsHelper
 
     def landing_page
-        render 'layouts/landing_page'
+        if current_user
+            render 'timelines/show'
+        else
+            render 'layouts/landing_page'
+        end
     end
 end
