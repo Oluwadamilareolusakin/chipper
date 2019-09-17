6ce6c9a77c27a956e0ecd8fd1d14f3ecd3b49cdb
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-17 03:00:10 +0100

    Add to correct user conditional for if user is an admin

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -23,7 +23,7 @@ module UsersHelper
 
     def correct_user?
         @user = User.find(params[:id])
-        redirect_to root_path unless current_user?(@user)
+        redirect_to root_path unless current_user?(@user) || current_user.is_admin
     end
     
 end
