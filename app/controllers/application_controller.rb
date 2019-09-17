05a3f410b627945df055a43001e0c0f6f9b4f798
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-17 22:29:06 +0100

    Remove landing page action from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
-
-    def landing_page
-        if current_user
-            render 'timelines/show'
-        else
-            render 'layouts/landing_page'
-        end
-    end
 end
