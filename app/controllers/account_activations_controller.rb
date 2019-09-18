8d07d765bc7243e0d8f7eb29c335c787658fe45d
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-19 00:51:17 +0100

    Move password activation out of helper

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,6 +3,18 @@ class AccountActivationsController < ApplicationController
 
     def edit
         @user = User.find_by( email: params[:email] )
-        attempt_to_activate(@user, params[:id])
+        # attempt_to_activate(@user, params[:id])
+        if user.activated
+            flash[:success] = "Your account has already been activated!"
+            redirect_to timeline_path
+        elsif user && user.authenticated?(params[:id],:activation)
+            activate user
+            login(user)
+            flash[:success] = "Your account has been activated successfully!"
+            redirect_to timeline_path
+        else
+            flash[:danger] = "Invalid Activation Link!"
+            redirect_to root_path
+        end 
     end
 end
--- app/helpers/account_activations_helper.rb
+++ app/helpers/account_activations_helper.rb
@@ -4,17 +4,6 @@ module AccountActivationsHelper
     end
 
     def attempt_to_activate(user, token)
-        if user.activated
-            flash[:success] = "Your account has already been activated!"
-            redirect_to timeline_path
-        elsif user && user.authenticated?(token,:activation)
-            activate user
-            login(user)
-            flash[:success] = "Your account has been activated successfully!"
-            redirect_to timeline_path
-        else
-            flash[:danger] = "Invalid Activation Link!"
-            redirect_to root_path
-        end 
+        
     end
 end
