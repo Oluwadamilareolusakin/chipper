f04861ed9ad9ba71ff4a74b120c774c7edf5f8c6
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-18 22:18:37 +0100

    Add user mailer, user account activations

---
+++ app/helpers/account_activations_helper.rb
@@ -0,0 +1,19 @@
+module AccountActivationsHelper
+    def activate(user)
+        user.activate_user
+    end
+
+    def attempt_to_activate(user, token)
+        if user.activated
+            flash[:success] = "Your account has been activated already!"
+        elsif user && user.authenticated?(token,:activation)
+            activate user
+            login(user)
+            flash[:success] = "Your account has been activated successfully!"
+            redirect_to timeline_path
+        else
+            flash[:danger] = "Invalid Activation Link!"
+            redirect_to root_path
+        end 
+    end
+end
---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UserMailerTest < ActionMailer::TestCase
+  setup do
+    @user = users(:jim)
+  end
+
+  test "account_activation" do
+    @user.activation_token = User.generate_token
+    mail = UserMailer.account_activation(@user)
+    assert_equal "Activate your Chipper Account", mail.subject
+    assert_equal [@user.email], mail.to
+    assert_equal ["from@example.com"], mail.from
+  end
+
+  test "password_reset" do
+    mail = UserMailer.password_reset
+    assert_equal "Password reset", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+end
