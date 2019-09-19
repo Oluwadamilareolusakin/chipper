d2ebf0ad5b7c7ef92d656811201fdd966e92c734
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-19 11:29:39 +0100

    Add attr_accessor for reset_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   has_many :posts, dependent: :destroy
   before_save :create_activation_token
   before_save :downcase_credentials
@@ -48,6 +48,11 @@ class User < ApplicationRecord
       return false if digest.nil?
 
       BCrypt::Password.new(digest).is_password?(token)
+    end
+
+    def create_reset_token
+      self.reset_token = User.generate_token
+      self.reset_digest = User.digest(reset_token)
     end
 
     def send_password_reset_email
