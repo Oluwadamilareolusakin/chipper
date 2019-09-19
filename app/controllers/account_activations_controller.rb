ad60e647aa4c3e9dc796a199c9dfbe0dacf29b0c
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-19 02:19:31 +0100

    rewrite authenticated? and update its use in sessions

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,19 +2,7 @@ class AccountActivationsController < ApplicationController
     include AccountActivationsHelper
 
     def edit
-        @@user = @user.find_by( email: params[:email] )
-        # attempt_to_activate(@@user, params[:id])
-        if @user.activated
-            flash[:success] = "Your account has already been activated!"
-            redirect_to timeline_path
-        elsif @user && @user.authenticated?(params[:id],:activation)
-            activate @user
-            login(@user)
-            flash[:success] = "Your account has been activated successfully!"
-            redirect_to timeline_path
-        else
-            flash[:danger] = "Invalid Activation Link!"
-            redirect_to root_path
-        end 
+        @user = User.find_by( email: params[:email] )
+        attempt_to_activate(@user, params[:id]) 
     end
 end
--- app/helpers/account_activations_helper.rb
+++ app/helpers/account_activations_helper.rb
@@ -4,6 +4,17 @@ module AccountActivationsHelper
     end
 
     def attempt_to_activate(user, token)
-        
+        if user.activated
+            flash[:success] = "Your account has already been activated!"
+            redirect_to timeline_path
+        elsif user 
+            activate user
+            login(user)
+            flash[:success] = "Your account has been activated successfully!"
+            redirect_to timeline_path
+        else
+            flash[:danger] = "Invalid Activation Link!"
+            redirect_to root_path
+        end 
     end
 end
