973d49303d1cfcad3cb2d0a4e97b7db69affcd29
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-19 13:32:34 +0100

    Formal arguments cannot be instance vars

--- app/helpers/account_activations_helper.rb
+++ app/helpers/account_activations_helper.rb
@@ -1,15 +1,15 @@
 module AccountActivationsHelper
-    def activate(@user)
-        @user.activate_@user
+    def activate(user)
+        user.activate_user
     end
 
-    def attempt_to_activate(@user, token)
-        if @user && @user.activated
+    def attempt_to_activate(user, token)
+        if user && user.activated
             flash[:success] = "Your account has already been activated!"
             redirect_to timeline_path
-        elsif @user && !@user.activated && @user.authenticated?(:activation, token)
-            activate @user
-            login(@user)
+        elsif user && !user.activated && user.authenticated?(:activation, token)
+            activate user
+            login(user)
             flash[:success] = "Your account has been activated successfully!"
             redirect_to timeline_path
         else
