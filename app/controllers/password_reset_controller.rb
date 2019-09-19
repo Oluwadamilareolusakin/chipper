4527303446a844e8c0132e06c432dce8503062f6
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-19 11:30:26 +0100

    setup new create edit and update methods for reset controller

---
+++ app/controllers/password_reset_controller.rb
@@ -0,0 +1,30 @@
+class PasswordResetController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:reset][:email])
+    if @user
+      @user.create_reset_token
+      @user.send_password_reset_email
+      flash[:success] = "Please check your email for password reset instructions"
+      redirect_to login_path
+    else
+      flash.now[:warning] = "No Chipper Account associated with that email"
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find_by(params[:email])
+    if !@user || !@user.authenticated?(:reset, params[:id])
+      flash[:danger] = "Invalid password reset link"
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(password_reset_params)
+  end
+end
